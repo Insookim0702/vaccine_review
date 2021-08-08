3eec5348b40a57f3fb451866ebf9bc83378eca04
insookim0702 <insookim0702@gmail.com>
2021-08-08 12:37:59 +0900

    새로고침 tab 버그 수정

--- src/global/TabMenu.tsx
+++ src/global/TabMenu.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import { Link } from "react-router-dom";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 
-export default function DisabledTabs() {
-  const [value, setValue] = React.useState(2);
+const MenuTabs = withRouter(({ location: { pathname } }) => {
+  const [value, setValue] = React.useState(0);
+  useEffect((): void => {
+    switch (pathname) {
+      case "/":
+        setValue(0);
+        break;
+      case "/observe":
+        setValue(1);
+        break;
+
+      case "/situation":
+        setValue(2);
+        break;
+
+      case "/my":
+        setValue(3);
+        break;
+    }
+  });
+  //TODO: 새로고침 했을 때, 컴포넌트 create때 경로에 맞는 tab이 표시되어야 함.
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
@@ -33,4 +52,6 @@ export default function DisabledTabs() {
       </Tabs>
     </Paper>
   );
-}
+});
+
+export default MenuTabs;
